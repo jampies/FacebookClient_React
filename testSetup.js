28ad21b2ec2402a48d06f6a5f17aed6bb26aaf53
Murray Le Roux <murray@avantgardeweb.co.uk>
2019-09-21 15:13:05 +0100

    Setup test runner

---
+++ testSetup.js
@@ -0,0 +1,31 @@
+require('@babel/register')();
+
+let Enzyme = require('enzyme');
+let Adapter = require('enzyme-adapter-react-16');
+
+Enzyme.configure({ adapter: new Adapter() });
+process.env.NODE_ENV = 'test';
+
+const { JSDOM } = require('jsdom');
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>', {
+  url: 'http://localhost'
+});
+const { window } = jsdom;
+
+function copyProps (src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .reduce((result, prop) => ({
+      ...result,
+      [prop]: Object.getOwnPropertyDescriptor(src, prop)
+    }), {});
+  Object.defineProperties(target, props);
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js'
+};
+copyProps(window, global);
